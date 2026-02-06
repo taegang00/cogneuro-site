d6e313cad99decf4f8f1e85f064c342571b9803c
taegang00 <taeganglee00@gmail.com>
2026-02-07 00:12:17 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
